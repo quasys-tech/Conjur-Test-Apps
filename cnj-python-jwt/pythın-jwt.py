d92097d97224e75bb9edc4df515d81aa212316f1
Emre Yardimci <emre.yardimci@quasys.com.tr>
2023-06-16 12:05:35 +0300

    python-jwt

--- cnj-python-jwt/pythın-jwt.py
+++ cnj-python-jwt/pythın-jwt.py
@@ -12,12 +12,12 @@ def get_password(secretPath):
         conjur_jwttoken = file.read().rstrip()
         
     conjur_authn_header = {'Content-Type':'application/x-www-form-urlencoded','Accept-Encoding': 'base64'}
-    authn_request = requests.post(conjur_baseurl + '/authn-jwt/' + conjur_serviceid +'/'+ conjur_account +'/authenticate',data='jwt='+ conjur_jwttoken, headers=conjur_authn_header)
+    authn_request = requests.post(conjur_baseurl + '/authn-jwt/' + conjur_serviceid +'/'+ conjur_account +'/authenticate',data='jwt='+ conjur_jwttoken, headers=conjur_authn_header, verify=False)
     print(authn_request.text)
     
     conjur_access_token = authn_request.text
     conjur_secret_header = "Authorization", "Token token=\"" + conjur_access_token.toString() + "\""
-    conjur_secret_get =  requests.get(conjur_baseurl + '/api/secrets/'+ conjur_account +'/variable/' + secretPath , headers=conjur_secret_header)
+    conjur_secret_get =  requests.get(conjur_baseurl + '/api/secrets/'+ conjur_account +'/variable/' + secretPath , headers=conjur_secret_header, verify=False)
     print(conjur_secret_get.text)
     return conjur_secret_get.text
 
