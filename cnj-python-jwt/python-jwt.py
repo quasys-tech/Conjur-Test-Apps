c59900312bc18590a406a59eeb25acec2d6b8d6f
Emre Yardimci <emre.yardimci@quasys.com.tr>
2023-06-16 12:57:07 +0300

    python-jwt

--- cnj-python-jwt/python-jwt.py
+++ cnj-python-jwt/python-jwt.py
@@ -2,20 +2,21 @@
 
 
 
-def get_secret(secretPath):
+def get_password(secretPath):
     conjur_baseurl = env["CONJUR_APPLIANCE_URL"]
     conjur_account = env["CONJUR_ACCOUNT"]
     conjur_serviceid = env["CONJUR_AUTHN_JWT_SERVICE_ID"]
+    cert_file = env["CONJUR_SSL_CERTIFICATE"]
     
     with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as file:
         conjur_jwttoken = file.read().rstrip()
         
-    conjur_authn_header = {'Content-Type':'application/x-www-form-urlencoded','Accept-Encoding': 'base64'}
+    conjur_authn_header = {'Content-Type': 'application/x-www-form-urlencoded','Accept-Encoding': 'base64'}
     authn_request = requests.post(conjur_baseurl + '/authn-jwt/' + conjur_serviceid +'/'+ conjur_account +'/authenticate',data='jwt='+ conjur_jwttoken, headers=conjur_authn_header, verify=False)
     print(authn_request.text)
     
     conjur_access_token = authn_request.text
-    conjur_secret_header = "Authorization", "Token token=\"" + conjur_access_token + "\""
+    conjur_secret_header = 'Authorization': 'Token token="' + conjur_access_token + '"'
     conjur_secret_get =  requests.get(conjur_baseurl + '/api/secrets/'+ conjur_account +'/variable/' + secretPath , headers=conjur_secret_header, verify=False)
     print(conjur_secret_get.text)
     return conjur_secret_get.text
@@ -23,4 +24,4 @@ def get_secret(secretPath):
 
 
 conjur_secretid = env["CONJUR_SECRET_ID"]
-password = get_secret(conjur_secretid)
+password = get_password(conjur_secretid)
